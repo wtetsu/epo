c86c376c8080d03462b6a416f63061acd36834af
wtetsu <tewatana@gmail.com>
2022-05-03 16:03:48 +0900

    Error exit code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod epo;
+use std::process::exit;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     match epo::arg::parse_arguments(&args) {
         Ok(settings) => epo::app::run(&settings),
-        Err(errors) => print_errors(&errors),
+        Err(errors) => {
+            print_errors(&errors);
+            exit(1);
+        }
     }
 }
 
