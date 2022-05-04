f7dabc30be7f07446bdb88307efff9c607eaa56d
wtetsu <tewatana@gmail.com>
2022-05-05 04:41:15 +0900

    help

--- src/epo/help.rs
+++ src/epo/help.rs
@@ -11,12 +11,13 @@ Examples:
   epo {epoch}
 
   epo {epoch} {epoch1} {epoch2}
-  epo now now+86400 now+86400*1
+  epo now now+86400 \"now+86400*2\"
 
   epo 0
   epo 0 -10 -5 +0 +5 +10
 
   epo now honolulu new_york london karachi sydney
+  epo \"[0,1,2,3,4,5,6].map(a=>now+86400*a)\" honolulu new_york london karachi sydney
   epo \"[...Array(365).keys()].map(a=>now+86400*a)\" denver phoenix los_angeles 
   "
   )
